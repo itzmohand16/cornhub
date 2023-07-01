48423111497a74a90e99781ba5f63469b7010ce5
itzmohand16 <137426396+itzmohand16@users.noreply.github.com>
2023-07-01 13:56:36 +0300

    Add files via upload

---
+++ New fucking project/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        backgroundImage: {
+            'fucking_subai': "url('fucking_files/fucking_dubai.jpg')",
+        },
+      extend: {},
+    },
+    plugins: [],
+  }
+  
